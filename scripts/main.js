6b857563e45b0f9a70e7c92f31440d245ff129f6
Awmni <none@nowhere.com>
2015-12-13 15:10:45 -0800

    updated h1 text

--- scripts/main.js
+++ scripts/main.js
@@ -22,7 +22,7 @@ if(!localStorage.getItem ('name')) {
   setUserName;
 } else {
   var storedName = localStorage.getItem('name');
-  myHeading.textContent = 'Mozilla is cool, ' + storedName;
+  myHeading.textContent = 'This is a test site, ' + storedName;
 }
 
 myButton.onclick = function() {
