70f11606a1caef2ae5c8672a75228ecf4320fdbf
Awmni <none@nowhere.com>
2015-12-13 14:59:02 -0800

    Changing image.

--- scripts/main.js
+++ scripts/main.js
@@ -2,10 +2,10 @@ var myImage = document.querySelector('img');
 
 myImage.onclick = function() {
   var mySrc = myImage.getAttribute('src');
-  if(mySrc === 'images/icicle.jpg') {
+  if(mySrc === 'images/Merry-Christmas.jpg') {
     myImage.setAttribute ('src', 'images/Merry-Christmas-holidyas.png');
   } else {
-    myImage.setAttribute ('src', 'images/icicle.jpg');
+    myImage.setAttribute ('src', 'images/Merry-Christmas.jpg');
   }
 }
 
